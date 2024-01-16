7c0d60165b1d85aee0d4f24a2e001367adb18312
menmkme <iahmad9963@gmail.com>
2024-01-16 12:26:15 +0100

    Database seeding and modification in user table

--- rms-api/database/factories/UserFactory.php
+++ rms-api/database/factories/UserFactory.php
@@ -24,21 +24,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'slug' => str::slug(Str::random(20)),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'is_admin' => false,
+            'image' => 'default.png',
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+   
 }
